9f64e17c7c9a3167a8232e2933d288d67b405e70
Hene-W <wannes.hene@gmail.com>
2026-03-04 12:27:29 +0100

    chore: initial backend setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cors from "cors" 
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+
+// Middlewares
+app.use(express.json())
+app.use(cors())
+
+// Route de test
+app.get('/', (req, res) => {
+    res.json({messag: "Backend is working!"})
+})
+
+// Lancer le server
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
