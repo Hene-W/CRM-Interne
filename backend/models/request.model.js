28db8adb394e4bd11ba99913462290e230711136
Hene-W <wannes.hene@gmail.com>
2026-03-05 12:51:09 +0100

    feat(request): add Request model

---
+++ backend/models/request.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose"
+
+const requestSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        clientName: {
+            type: String,
+            required: true
+        },
+        firstName: {
+            type: String,
+        },
+        email: {
+            type: String,
+        },
+        requestType: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            default: "En cours de traitement"
+        },
+        internalNotes: {
+            type: String,
+            default: ""
+        }
+    }, {
+    timestamps: true,
+}
+)
+
+const Request = mongoose.model("Request", requestSchema)
+
+export default Request
