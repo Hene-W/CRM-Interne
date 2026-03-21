1f91da8590003569c1b8bbdbc3941be724b64d63
Hene-W <wannes.hene@gmail.com>
2026-03-21 16:55:01 +0100

    feat(dashboard): add logic to calculate total and per-status counts from requests context

--- frontend/src/component/DashboardItem.jsx
+++ frontend/src/component/DashboardItem.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { statusColors } from "../constants/statusColors";
 
-const DashboardItem = ({ status, number }) => {
+const DashboardItem = ({ status, count }) => {
     
 
     return (
         <div>
             <div className={`${statusColors[status] || "bg-gray-100 text-gray-700"} p-4 rounded-lg space-y-2 shadow`}>
                 <p className="text-sm">{status}</p>
-                <p className="text-xl font-bold text-black">{number}</p>
+                <p className="text-xl font-bold text-black">{count}</p>
             </div>
         </div>
     )
--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import Header from "../component/Header"
 import DashboardItem from '../component/DashboardItem'
+import { useRequests } from '../context/RequestContext'
+
 
 const DashboardPage = () => {
+  const { requests } = useRequests()
+
+  const totalRequests = requests?.length
+
+  const statusCounts = requests.reduce((acc, req) => {
+    acc[req.status] = (acc[req.status] || 0) + 1
+    return acc
+  }, {})
+
+  const statuses = ["Nouveau", "En cours", "Terminé", "Refusé"];
+
   return (
     <div>
       <Header title="Dashboard" showButton={false} />
@@ -11,15 +24,18 @@ const DashboardPage = () => {
         {/* Total des demandes */}
         <div className="bg-[#1f1f1f] text-white p-6 rounded-lg shadow-md text-center space-y-2">
           <p className="text-sm">Total des demandes</p>
-          <p className="text-3xl font-bold">15</p>
+          <p className="text-3xl font-bold">{totalRequests}</p>
         </div>
 
         {/* Statuts */}
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-          <DashboardItem status="Nouveau" number="13" />
-          <DashboardItem status="Terminé" number="13" />
-          <DashboardItem status="En cours" number="13" />
-          <DashboardItem status="Refusé" number="13" />
+          {statuses.map(status => (
+            <DashboardItem
+              key={status}
+              status={status}
+              count={statusCounts[status] || 0}
+            />
+          ))}
         </div>
       </div>
     </div>
