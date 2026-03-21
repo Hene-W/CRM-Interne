cc7d53609d36c2c651296ef920737aede3cd15d6
Hene-W <wannes.hene@gmail.com>
2026-03-21 15:00:57 +0100

    feat(sidebar): add sidebar header with title

--- frontend/src/component/Sidebar.jsx
+++ frontend/src/component/Sidebar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import SidebarHeader from './Sidebar/SidebarHeader'
+import SidebarContent from './Sidebar/SidebarContent'
 
 const Sidebar = () => {
   return (
-    <div className=''>Sidebar</div>
+    <div className='p-4 flex flex-col gap-2'>
+      <SidebarHeader />
+      <SidebarContent />
+    </div>
   )
 }
 
---
+++ frontend/src/component/Sidebar/SidebarHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SidebarHeader = () => {
+  return (
+    <div className='border-b pb-4'>
+        <h1 className='text-xl font-semibold'>CRM Interne</h1>
+    </div>
+  )
+}
+
+export default SidebarHeader
